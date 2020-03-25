e7ce4aafd0d6068ef121f3c4eebb0e4636a6ea8f
Yersultan Medetov <y.medetov@gmail.com>
2020-03-25 22:18:36 +0600

    add new ingredient

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -5,7 +5,11 @@ import lombok.*;
 import javax.persistence.*;
 import java.math.BigDecimal;
 
-@Data
+/**
+ * Created by jt on 6/13/17.
+ */
+@Getter
+@Setter
 @EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Ingredient {
